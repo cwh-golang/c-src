7b378a4b6878f006cd41490bb77834605fc9d113
xiaolaoshu <38762746+cwh-golang@users.noreply.github.com>
2018-05-30 13:34:09 +0800

    secondprogram
    c program

---
+++ secondprogram/secondprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int a = 0;
+	printf("请输入参数的值：\n");
+	scanf("%d",&a);
+	printf("%d\n", a);
+	printf("This is a wonderful world!\n");
+	int b = 6;
+	int c = 7;
+	printf("%d\n", b + c);
+    return 0;
+}
