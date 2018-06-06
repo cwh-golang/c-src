a9b35799f20170ef1b3ff974ba46f2f12e3de82f
cwh-golang <caoweihua288@sina.cn>
2018-06-06 16:59:20 +0800

    1、增加了数据类型部分的代码 2、增加了“使用git bash命令进行 GitHub管理”部分

--- constval/constval.c
+++ constval/constval.c
@@ -16,6 +16,14 @@ int main(int argc, char const *argv[])
 	printf("%d\n", sizeof(char));
 	printf("%d\n", sizeof(long long));
 	printf("%d\n", sizeof(double));
-		
+	printf("%d\n", sizeof(short));
+	printf("%u\n", 55);
+	unsigned int k = -1;
+	printf("%d\n", k);
+	printf("%u\n", k);
+	printf("%d,%c\n", 63,63);
+	printf("%d,%c\n", 128,128);
+	char k_char = 254;
+	printf("%d,%c\n", k_char,k_char);
 	return 0;
 }
